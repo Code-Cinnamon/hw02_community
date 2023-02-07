8c6af94a25ac8cae2495907e7b95946892a04d60
Artem <art-89@yandex.ru>
2023-02-07 23:06:53 +0300

    attempt_2

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig',
+    'posts.apps.PostsConfig', #тут все ок
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
