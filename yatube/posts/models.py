bec5b91097e69695bff801371f0ff070b42c244f
Artem <art-89@yandex.ru>
2023-02-09 00:08:40 +0300

    review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -26,7 +26,8 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts'
+        related_name='posts',
     )
+
     class Meta:
         ordering = ("-pub_date")
